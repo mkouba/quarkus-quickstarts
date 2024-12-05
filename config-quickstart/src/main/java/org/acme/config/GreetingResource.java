09132f8dd6d4ca35ce5cd97473de4eac47d2eafd
Martin Kouba <mkouba@redhat.com>
2024-12-05 11:09:26 +0100

    Reproducer

--- config-quickstart/src/main/java/org/acme/config/GreetingResource.java
+++ config-quickstart/src/main/java/org/acme/config/GreetingResource.java
@@ -2,12 +2,15 @@ package org.acme.config;
 
 import java.util.Optional;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import io.quarkus.runtime.ApplicationConfig;
+import io.quarkus.vertx.http.runtime.HttpBuildTimeConfig;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/greeting")
 public class GreetingResource {
@@ -21,9 +24,21 @@ public class GreetingResource {
     @ConfigProperty(name = "greeting.name")
     Optional<String> name;
 
+    @Inject
+    ApplicationConfig appConfig;
+
+    @Inject
+    HttpBuildTimeConfig httpConfig;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return message + " " + name.orElse("world") + suffix;
+    }
+
+    @GET
+    @Path("config")
+    public String config() {
+        return String.format("rootPath: %s, appName: %s", httpConfig.rootPath, appConfig.name().orElse("n/a"));
     }
 }
