a5c25184644aec69be18cf6b428397d6c7a91969
Martin Kouba <mkouba@redhat.com>
2025-10-20 11:43:16 +0200

    Cache quickstart: add reproducer for #50513

--- cache-quickstart/src/main/java/org/acme/cache/WeatherForecastResource.java
+++ cache-quickstart/src/main/java/org/acme/cache/WeatherForecastResource.java
@@ -3,18 +3,32 @@ package org.acme.cache;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import org.jboss.resteasy.reactive.RestQuery;
+
+import io.quarkus.cache.Cache;
+import io.quarkus.cache.CacheName;
+import io.quarkus.logging.Log;
+import io.smallrye.mutiny.Uni;
+import jakarta.annotation.PreDestroy;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-
-import org.jboss.resteasy.reactive.RestQuery;
 
 @Path("/weather")
 public class WeatherForecastResource {
 
     @Inject
     WeatherForecastService service;
+
+    @CacheName("TEST_DATA")
+    Cache cache;
+
+    ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
     @GET
     public WeatherForecast getForecast(@RestQuery String city, @RestQuery long daysInFuture) {
@@ -26,4 +40,25 @@ public class WeatherForecastResource {
         long executionEnd = System.currentTimeMillis();
         return new WeatherForecast(dailyForecasts, executionEnd - executionStart);
     }
+
+    @GET
+    @Path("foo")
+    public Uni<List<String>> test() {
+        Log.info("::test::");
+        return cache.getAsync("all", key -> {
+            Log.info("::cache_miss::");
+
+            return Uni.createFrom().emitter(em -> {
+                executor.schedule(() -> {
+                    em.complete(List.of("all", "test", UUID.randomUUID().toString()));
+                }, 100, TimeUnit.MILLISECONDS); // 
+            });
+        });
+    }
+
+    @PreDestroy
+    void destroy() {
+        executor.shutdown();
+    }
+
 }
